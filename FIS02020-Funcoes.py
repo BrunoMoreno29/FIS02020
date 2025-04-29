b162bac03570727aced5a5300cda9c820bb03b40
BrunoMoreno29 <bruno.moreno.morales@gmail.com>
2025-04-29 16:34:19 -0300

    Primeiro commit

---
+++ FIS02020-Funcoes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 22 16:33:47 2025
+
+@author: bmorales
+"""
+
+#Funções
+
+def sayHello():
+    print('Hello !')
+
+sayHello()
+
+def quadrado(x):
+    return x**2
+
+print(quadrado(5))
+
+def soma(a,b,c):
+    s = a+b+c
+    return s
+
+print(soma(10,15,17))
